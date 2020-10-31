ec525adbde8bac4b9c00fb2fbd7ef0f0159b9c9f
School <School@DESKTOP-JLRVCKH>
2020-10-31 14:59:39 -0500

    Finished for Assignment 5

--- CSCEProject3/com/bc/ext/DatabaseInfo.java
+++ CSCEProject3/com/bc/ext/DatabaseInfo.java
@@ -1,8 +1,11 @@
 package com.bc.ext;
 
 public class DatabaseInfo {
-	public static final String USERNAME = "jstollar";
-	public static final String PASSWORD = "YYHNepGT7";
+	//public static final String USERNAME = "jstollar";
+	//public static final String PASSWORD = "YYHNepGT7";
+	
+	public static final String USERNAME = "sduncan";
+	public static final String PASSWORD = "DhtrGgT0";
 	public static final String URL = "jdbc:mysql://cse.unl.edu/" + USERNAME + "?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
 }
 
