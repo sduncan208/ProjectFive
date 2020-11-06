a1e52439b2e5b24d807ca7606fea09dae8aba5b5
Sophia <School@DESKTOP-JLRVCKH.unl.edu>
2020-11-06 16:29:43 -0600

    Adding nodes and an abstracted list

---
+++ CSCEProject3/com/bc/InvoiceListNode.java
@@ -0,0 +1,24 @@
+package com.bc;
+
+public class InvoiceListNode {
+
+	private InvoiceListNode next;
+	private Invoice item;
+
+	public InvoiceListNode(Invoice item) {
+		this.item = item;
+		this.next = null;
+	}
+
+	public Invoice getInvoice() {
+		return item;
+	}
+
+	public InvoiceListNode getNext() {
+		return next;
+	}
+
+	public void setNext(InvoiceListNode next) {
+		this.next = next;
+	}
+}
